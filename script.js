f245929ba63fbcaf082eef01faa6cd8372603c76
trvannhanh <trvannhanh@gmail.com>
2023-07-31 16:45:32 +0700

    update header & navbar

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.getElementById("prev").onclick = function(){
     const widthItem = document.querySelector('.item').offsetWidth;
     document.getElementById("formList").scrollLeft -= widthItem;
 }
+
+
+
+const navBar = document.getElementById('nav-bar-container');
+const logo = document.querySelector('#nav-bar-container .logo-fullscreen');
+window.addEventListener('scroll', function(){
+    const scrollY = window.scrollY;
+    if (scrollY === 0) {
+        navBar.classList.remove('fixed');
+        logo.style.visibility = "hidden";
+    } else{
+        navBar.classList.add('fixed');
+        logo.style.visibility = "inherit";
+    }
+});
